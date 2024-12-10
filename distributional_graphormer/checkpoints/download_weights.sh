0c5c5d68f48fe42b1d47f214420c55c08bd9e990
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-12-10 13:44:25 -0600

    added dockerfile, modified install.sh and made a sh file to download the model's weights

---
+++ distributional_graphormer/checkpoints/download_weights.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget "https://huggingface.co/microsoft/Graphormer/resolve/main/Distributional-Graphormer/protein/checkpoints/checkpoint-520k.pth?download=true" -O checkpoint-520k.pth
